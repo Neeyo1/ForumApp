3e22e54ef11d763f4d2d73f6edbbcbf293f1a40b
Neeyo <jakub.nycz99@gmail.com>
2024-09-10 09:35:09 +0200

    Create register and login functionality

--- API/Program.cs
+++ API/Program.cs
@@ -1,7 +1,12 @@
+using System.Text;
 using API.Data;
 using API.Entities;
+using API.Interfaces;
+using API.Services;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.IdentityModel.Tokens;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -12,6 +17,8 @@ builder.Services.AddDbContext<DataContext>(opt =>
 {
     opt.UseSqlite(builder.Configuration.GetConnectionString("DefaultConnection"));
 });
+builder.Services.AddScoped<ITokenService, TokenService>();
+builder.Services.AddAutoMapper(AppDomain.CurrentDomain.GetAssemblies());
 
 builder.Services.AddIdentityCore<AppUser>(opt => 
     {
@@ -20,6 +27,21 @@ builder.Services.AddIdentityCore<AppUser>(opt =>
         .AddRoles<AppRole>()
         .AddRoleManager<RoleManager<AppRole>>()
         .AddEntityFrameworkStores<DataContext>();
+
+builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
+    .AddJwtBearer(options =>
+        {
+            var tokenKey = builder.Configuration["TokenKey"] ?? 
+                throw new Exception("Token key not found");
+            options.TokenValidationParameters = new TokenValidationParameters
+            {
+                ValidateIssuerSigningKey = true,
+                IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(tokenKey)),
+                ValidateIssuer = false,
+                ValidateAudience = false
+            };
+        }
+    );
 
 var app = builder.Build();
 
